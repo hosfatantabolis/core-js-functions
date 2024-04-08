d4e82ca49942b46a2c6a23796fbf563b67d417bd
Maksim Litvinov <lmk478shade@gmail.com>
2024-04-08 22:52:07 +0300

    feat: solve getPowerFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -75,8 +75,11 @@ function getArgumentsCount(funcs) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  function exp(x) {
+    return x ** exponent;
+  }
+  return exp;
 }
 
 /**
